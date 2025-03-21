582a667f84cedbfbe095ba17945bb500dda25f06
Mihail Tsankov <20221@ukt-bg.com>
2025-03-21 08:38:15 +0200

    commit all tests 3

--- load-testing/smoke.js
+++ load-testing/smoke.js
@@ -1,21 +1,15 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const URL = 'https://http://localhost:8080/index.html';
-
-function validateResponse(res) {
-  check(res, { 'status is 200': (r) => r.status === 200 });
-  sleep(Math.random() * 2);
-}
-
-// Smoke Test
-export const smokeTest = {
-  stages: [
-    { duration: '1m', target: 5 },
-  ],
+export let options = {
+  vus: 5,
+  duration: '1m',
 };
 
-export function smoke() {
-  const res = http.get(URL);
-  validateResponse(res);
+export default function () {
+  let res = http.get('https://localhost:8080/');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1);
 }
